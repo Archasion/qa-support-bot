04c8a63d3f7be148110047c1360287623d558647
Archasion <james.nswork@gmail.com>
2022-01-20 17:24:07 +0000

    Updated structure

--- src/logger.js
+++ src/logger.js
@@ -7,21 +7,26 @@ module.exports = new Logger({
 	levels: {
 		_logger: { format: "&f&!7{timestamp}&r [LOGGER] {text}" },
 		basic: { format: "&f&!7{timestamp} {text}" },
+
 		commands: {
 			format: "&f&!7{timestamp}&r &3[INFO] &d(COMMANDS)&r {text}",
 			type: "info"
 		},
+
 		console: { format: "&f&!7{timestamp} [INFO] {text}" },
 		debug: { format: "&f&!7{timestamp}&r &1[DEBUG] &9{text}" },
 		error: { format: "&f&!7{timestamp}&r &4[ERROR] &c{text}" },
+
 		http: {
 			format: "&f&!7{timestamp}&r &3[INFO] &d(HTTP)&r {text}",
 			type: "info"
 		},
+
 		info: { format: "&f&!7{timestamp}&r &3[INFO] &b{text}" },
 		notice: { format: "&f&!7{timestamp}&r &0&!6[NOTICE] {text}" },
 		success: { format: "&f&!7{timestamp}&r &2[SUCCESS] &a{text}" },
 		warn: { format: "&f&!7{timestamp}&r &6[WARN] &e{text}" },
+
 		ws: {
 			format: "&f&!7{timestamp}&r &3[INFO] &d(WS)&r {text}",
 			type: "info"
