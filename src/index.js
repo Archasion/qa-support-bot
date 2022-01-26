719243adeb455131b7151ae2ea5c57f9507bc139
Archasion <james.nswork@gmail.com>
2022-01-26 09:15:56 +0000

    Removed unecessary code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-module.exports = (req, res) => {
-	const { name = "World" } = req.query;
-	res.send(`Hello ${name}!`);
-};
-
 // ANCHOR Constants
 const express = require("express");
 const yaml = require("js-yaml");
