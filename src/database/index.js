b5bd9ec4b5329dae087c5d166c1d9a4f41d87162
Archasion <james.nswork@gmail.com>
2022-02-05 14:03:32 +0000

    Updated DB configuration (will slowly switch to mongoDB)

--- src/database/index.js
+++ src/database/index.js
@@ -1,48 +1,16 @@
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const { path } = require("../utils/fs");
-const types = require("./dialects");
 
 module.exports = async client => {
-	const { DB_TYPE, DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME } = process.env;
-	const type = (DB_TYPE || "sqlite").toLowerCase();
+	require("sqlite3");
+	log.info("Using SQLite storage");
 
-	const supported = Object.keys(types);
-
-	if (!supported.includes(type)) {
-		log.error(new Error(`DB_TYPE (${type}) is not a valid type`));
-		return process.exit();
-	}
-
-	try {
-		types[type].packages.forEach(pkg => require(pkg));
-	} catch {
-		const required = types[type].packages.map(i => `"${i}"`).join(" and ");
-		log.error(
-			new Error(`Please install the package(s) for your selected database type: ${required}`)
-		);
-		return process.exit();
-	}
-
-	let sequelize;
-
-	if (type === "sqlite") {
-		log.info("Using SQLite storage");
-		sequelize = new Sequelize({
-			dialect: types[type].dialect,
-			logging: text => log.debug(text),
-			storage: path("./database.sqlite")
-		});
-		log.warn("Message logging is disabled due to insufficient database");
-	} else {
-		log.info(`Connecting to ${types[type].name} database...`);
-		sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
-			dialect: types[type].dialect,
-			host: DB_HOST,
-			logging: text => log.debug(text),
-			port: DB_PORT
-		});
-	}
+	const sequelize = new Sequelize({
+		dialect: "sqlite",
+		logging: text => log.debug(text),
+		storage: path("./database.sqlite")
+	});
 
 	try {
 		await sequelize.authenticate();
