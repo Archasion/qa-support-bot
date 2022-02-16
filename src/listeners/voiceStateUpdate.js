5b8e7897746727d9ef98d8af982b890465140449
Archasion <james.nswork@gmail.com>
2022-02-16 09:13:21 +0000

    Removed staff validation

--- src/listeners/voiceStateUpdate.js
+++ src/listeners/voiceStateUpdate.js
@@ -30,22 +30,20 @@ module.exports = class VoiceStateUpdateEventListener extends EventListener {
 			);
 		}
 
-		if (!(await utils.isStaff(newMember))) {
-			// No-mic for public VC
-			if (oldState === publicVC || oldState === publicTestingVC) {
-				await publicNoMic.permissionOverwrites.create(user, {
-					VIEW_CHANNEL: true,
-					SEND_MESSAGES: true
-				});
-			} else hideChannel(publicNoMic);
+		// No-mic for public VC
+		if (oldState === publicVC || oldState === publicTestingVC) {
+			await publicNoMic.permissionOverwrites.create(user, {
+				VIEW_CHANNEL: true,
+				SEND_MESSAGES: true
+			});
+		} else hideChannel(publicNoMic);
 
-			// No-mic for NDA VC
-			if (oldState === NDA_CHAT_VC || oldState === NDA_TESTING_VC) {
-				await NDANoMic.permissionOverwrites.create(user, {
-					VIEW_CHANNEL: true,
-					SEND_MESSAGES: true
-				});
-			} else hideChannel(NDANoMic);
-		}
+		// No-mic for NDA VC
+		if (oldState === NDA_CHAT_VC || oldState === NDA_TESTING_VC) {
+			await NDANoMic.permissionOverwrites.create(user, {
+				VIEW_CHANNEL: true,
+				SEND_MESSAGES: true
+			});
+		} else hideChannel(NDANoMic);
 	}
 };
