ce2284c626c45f16a63d7e1484d30ff35ec972bd
Archasion <james.nswork@gmail.com>
2022-02-21 22:08:08 +0000

    Check if the event still exists

---
+++ src/listeners/guildScheduledEventUpdate.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-useless-escape */
+const EventListener = require("../modules/listeners/listener");
+
+const { MODERATION_CHAT, ACTIVE_TESTING_REQUESTS } = process.env;
+
+module.exports = class GuildScheduledEventUpdateEventListener extends EventListener {
+	constructor(client) {
+		super(client, { event: "guildScheduledEventUpdate" });
+	}
+
+	async execute(oldEvent, newEvent) {
+		console.log(oldEvent);
+		console.log(newEvent);
+		if (!newEvent) {
+			// prettier-ignore
+			oldEvent.guild.channels.cache.get(MODERATION_CHAT).messages.fetch(ACTIVE_TESTING_REQUESTS)
+				.then(async message => {
+					// Get the test in the message
+					// prettier-ignore
+					const replaceRegex = new RegExp(`\\n\\n>\\s[^\\n]+<t:${oldEvent.scheduledStartTimestamp / 1000}:F>\\n>\\shttps:\/\/discord\.com\/channels(?:\/\\d{17,19}){3}`, "gmis");
+
+					// Remove the test from the message
+					message.edit({ content: message.content.replace(replaceRegex, "") });
+				});
+		}
+	}
+};
