69a752de46b0ab9ec3ebe3c4bd13c9fa7a54ba79
Archasion <james.nswork@gmail.com>
2022-02-21 16:30:35 +0000

    Fixed regex for `guildScheduledEventDelete`

--- src/listeners/guildScheduledEventDelete.js
+++ src/listeners/guildScheduledEventDelete.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-useless-escape */
 const EventListener = require("../modules/listeners/listener");
 
-const { MODERATION_CHAT, ACTIVE_TESTING_REQUESTS, NDA_TESTING_VC } = process.env;
+const { MODERATION_CHAT, ACTIVE_TESTING_REQUESTS } = process.env;
 
 module.exports = class GuildScheduledEventDeleteEventListener extends EventListener {
 	constructor(client) {
@@ -14,12 +14,7 @@ module.exports = class GuildScheduledEventDeleteEventListener extends EventListe
 			.then(async message => {
 				// Get the test in the message
 				// prettier-ignore
-				const replaceRegex = new RegExp(
-					`\n\n>\\s${event.channel.id === NDA_TESTING_VC ? "<:nda:905799212350992475>" : ""}.+<t:${
-						event.scheduledStartTimestamp / 1000
-					}:F>\n>\\shttps:\/\/discord\.com\/channels(?:\/\\d{17,19}){3}`,
-					"gmis"
-				);
+				const replaceRegex = new RegExp(`\\n\\n>\\s[^\\n]+<t:${event.scheduledStartTimestamp / 1000}:F>\\n>\\shttps:\/\/discord\.com\/channels(?:\/\\d{17,19}){3}`, "gmis");
 
 				// Remove the test from the message
 				message.edit({ content: message.content.replace(replaceRegex, "") });
