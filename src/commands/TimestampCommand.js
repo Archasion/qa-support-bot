0716fbd867b74f901126ff4c87d74aa47b1a7b25
Archasion <james.nswork@gmail.com>
2022-02-22 23:48:41 +0000

    Added a timestamp command

---
+++ src/commands/TimestampCommand.js
@@ -0,0 +1,28 @@
+const Command = require("../modules/commands/command");
+
+module.exports = class TimestampCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: "timestamp",
+			description: "Creates a timestamp.",
+			permissions: [],
+			ignored: {
+				roles: [],
+				channels: [],
+				threads: []
+			}
+		});
+	}
+
+	/**
+	 * @param {Interaction} interaction
+	 * @returns {Promise<void|any>}
+	 */
+	async execute(interaction) {
+		interaction.reply({
+			content:
+				"Feel free to use website to build timestamps: https://archasion.github.io/Discord-Timestamp-Builder/",
+			ephemeral: true
+		});
+	}
+};
