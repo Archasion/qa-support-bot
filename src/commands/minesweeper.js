2dba82b74d9c524eafc64b6889cec78eef89f2a6
Archasion <james.nswork@gmail.com>
2022-02-24 20:26:29 +0000

    Added `/minesweeper`

--- src/commands/minesweeper.js
+++ src/commands/minesweeper.js
@@ -1,10 +1,10 @@
 const Command = require("../modules/commands/command");
 
-module.exports = class TimestampCommand extends Command {
+module.exports = class MinesweeperCommand extends Command {
 	constructor(client) {
 		super(client, {
-			name: "timestamp",
-			description: "Links to a website that builds timestamps.",
+			name: "minesweeper",
+			description: "Links to the minesweeper website.",
 			permissions: [],
 			verified_only: true,
 			ignored: {
@@ -22,7 +22,7 @@ module.exports = class TimestampCommand extends Command {
 	async execute(interaction) {
 		interaction.reply({
 			content:
-				"Feel free to use this website to build timestamps (no mobile support):\nhttps://archasion.github.io/Discord-Timestamp-Builder/",
+				"Feel free to use this website to play minesweeper (no mobile support):\nhttps://archasion.github.io/Minesweeper/",
 			ephemeral: true
 		});
 	}
