8c36cf794166b7f357a3599b959cf74ac0063c89
Archasion <james.nswork@gmail.com>
2022-02-25 22:49:45 +0000

    Allowed active testers to use `/ticket`

--- src/commands/TicketCommand.js
+++ src/commands/TicketCommand.js
@@ -20,6 +20,7 @@ module.exports = class TicketCommand extends Command {
 			},
 			// verified_only: true,
 			nda_only: true,
+			active_only: true,
 			options: [
 				{
 					description: "Create a new ticket",
