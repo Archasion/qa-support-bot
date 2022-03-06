38e0400efc72fdc1110ecdf86aa3bca192cb4fd1
Archasion <james.nswork@gmail.com>
2022-03-06 18:45:44 +0000

    Fixed validation

--- src/commands/SessionCommand.js
+++ src/commands/SessionCommand.js
@@ -104,16 +104,19 @@ module.exports = class SessionCommand extends Command {
 		let announcementChannel = null;
 
 		// Check whether it is too early to post the announcement
-		if (type && !forceAnnounce) {
+		if (type) {
 			try {
 				// Get the announcement
-				announcement = embed.fields
-					.filter(
+				announcement = embed.fields;
+				if (!forceAnnounce) {
+					announcement = announcement.filter(
 						field =>
 							field.name.includes(type) &&
 							field.name.slice(type.length + 4, -3) <= parseInt(Date.now() / 1000)
-					)[0]
-					.value.split("```")[1];
+					)[0];
+				}
+
+				announcement = announcement.value.split("```")[1];
 			} catch {
 				interaction.reply({
 					content: "It is too early to post the anouncement",
